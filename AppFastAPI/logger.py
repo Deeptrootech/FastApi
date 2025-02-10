a3c514a8a9af50558733e0a3804fdfc6e54e82a6
Deep Pathak <deep.pathak@trootech.com>
2025-02-10 16:42:28 +0530

    set logger file

--- AppFastAPI/logger.py
+++ AppFastAPI/logger.py
@@ -1,8 +1,12 @@
 import logging
 
 # Set up global logger
+fileHandler = logging.FileHandler("my_log.log")
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.ERROR,
     format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        fileHandler,
+    ]
 )
 logger = logging.getLogger("simple_logger")
