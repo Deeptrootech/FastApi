a9b1a1931473f5b3d1543574fe3952f407e0624b
Deep Pathak <deep.pathak@trootech.com>
2025-02-13 18:31:10 +0530

    git inore migration version and post model field add

--- AppFastAPI/models/posts.py
+++ AppFastAPI/models/posts.py
@@ -13,8 +13,6 @@ class Post(Base):
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default='TRUE')
+    genre = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'))
-    # updated_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'))
-
-
-    #TODO: uncomment this and start checking for each command
+    updated_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'))
