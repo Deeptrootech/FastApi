46b670c3cd8e344b9861fc629692e5773f453120
Deep Pathak <deep.pathak@trootech.com>
2026-04-27 18:05:37 +0530

    wip on multi tenant arch

--- api/organization.py
+++ api/organization.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
+from dependency.db_dependencies import get_global_db
 from models.global_models import Organizations
 
 router = APIRouter()
 
 
-@router.get("/organizations", response_model=list[Organizations])
-def get_organizations(organization_name: str, db: Session = Depends(get_db)):
+@router.get("/organizations")
+def get_organizations(organization_name: str, db: Session = Depends(get_global_db)):
     org = db.query(Organizations).filter(Organizations.name == organization_name).first()
     if not org:
         raise HTTPException(status_code=404, detail="Organization not found")
---
+++ tenant_config/create_database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine, text
+
+
+def create_database(db_name: str):
+    engine = create_engine("postgresql://deep:1234@localhost:5432/postgres")
+
+    with engine.connect() as conn:
+        conn.execution_options(isolation_level="AUTOCOMMIT")
+        conn.execute(text(f'CREATE DATABASE "{db_name}"'))
